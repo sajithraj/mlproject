fb0516ae03bee306c2c922d8092e1e41325a07a6
sajithraj <sajithraj3@gmail.com>
2023-03-11 21:49:00 +0530

    added the src

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup 
+from typing import List
+
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    FUNTIN TO READ THE REQUIREMTNS AND RETURN AS LIST
+    '''
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [ req.replace("\n" , "") for req in requirements]
+
+        print ( " ************ " , requirements)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        print ( " ************ " , requirements)
+    
+    return requirements
+
+
+setup(
+name ='mlproject',
+version='0.0.1',
+author='Sajithraj C',
+author_email= 'sajithraj3@gmail.com',
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
